3925e9d1ab9834ca92ab94405a7e56a22e03a85f
NuvoloniM <nuvoloni.m@gmail.com>
2022-05-23 18:02:10 +0200

    shop section

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -14,9 +14,12 @@
     <!-- main -->
     <main>
 
+        <!-- sezione jumbotron -->
         @include('partials.hero')
-    
+        <!-- seztione del contenuto principale, uso yield perchè varia a seconda della pagina -->
         @yield('content')
+        <!-- sezione link per lo shop -->
+        @include('partials.shop')
     </main>
 
 </body>
---
+++ resources/views/partials/shop.blade.php
@@ -0,0 +1,26 @@
+<section class="shop">
+    <div class="row">
+        <ul>
+            <li>
+                <img src=" {{ asset('images/buy-comics-digital-comics.png') }} " alt="">
+                <a href=""> digital comics</a>
+            </li>
+            <li>
+                <img src="{{ asset('images/buy-comics-merchandise.png') }}" alt="">
+                <a href=""> dc merchandise</a>
+            </li>
+            <li>
+                <img src="{{ asset('images/buy-comics-subscriptions.png') }}" alt="">
+                <a href=""> subscription</a>
+            </li>
+            <li>
+                <img src="{{ asset('images/buy-comics-shop-locator.png') }}" alt="">
+                <a href=""> comic shop locator</a>
+            </li>
+            <li>
+                <img src="{{ asset('images/buy-dc-power-visa.svg') }}" alt="">
+                <a href=""> dc power visa</a>
+            </li>
+        </ul>
+    </div>
+</section>
