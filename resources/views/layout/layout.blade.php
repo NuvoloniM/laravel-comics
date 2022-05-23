991d7c5081b6388cecc0b629e06f570675b08ea2
NuvoloniM <nuvoloni.m@gmail.com>
2022-05-23 16:33:32 +0200

    Header

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+    <!-- collego file stile che si crea in public -->
+    <link rel="stylesheet" href=" {{ asset('css/app.css') }}">
     <title>DC Comics : @yield('title')</title>
 </head>
 <body>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,52 @@
 <header>
-    <div class="container">
-        <div> dcpower<span>&#8480;</span>visa&reg;</div>
-        <div></div>
+    <div class="row">
+        <div class="head-menu">
+            <div> dcpower<span>&#8480;</span>visa&reg;</div>
+            <div> additional dc sites &#9660;</div>
+        </div>
+    </div>
+    <div class="navbar">
+        <div class="row">
+            <div class="logo">
+                <img src="{{ asset('images/dc-logo.png') }}" alt="logo">
+            </div>
+            <div class="nav">
+                <ul>
+                    <li>
+                        <a href=""> characters</a>
+                    </li>
+                    <li class="current">
+                        <a href="" > comics</a>
+                    </li>
+                    <li>
+                        <a href=""> movies</a>
+                    </li>
+                    <li>
+                        <a href=""> tv</a>
+                    </li>
+                    <li>
+                        <a href=""> games</a>
+                    </li>
+                    <li>
+                        <a href=""> collectibles</a>
+                    </li>
+                    <li>
+                        <a href=""> videos</a>
+                    </li>
+                    <li>
+                        <a href=""> fans</a>
+                    </li>
+                    <li>
+                        <a href=""> news</a>
+                    </li>
+                    <li>
+                        <a href=""> shop <span>&#9660;</span></a>
+                    </li>
+                </ul>
+            </div>
+            <div class="search">
+                <input type="text" placeholder="Search">
+            </div>
+        </div>
     </div>
 </header>
