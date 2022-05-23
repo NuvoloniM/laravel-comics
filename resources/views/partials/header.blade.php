6149979e8e1bc45cce603c5cdb7ef0885c4117de
NuvoloniM <nuvoloni.m@gmail.com>
2022-05-23 15:54:48 +0200

    header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,6 @@
 <header>
-    <div>
-        <div class="container">
-            <div> dcpower<span>&#8480;</span>visa&reg;</div>
-            <div></div>
-        </div>
+    <div class="container">
+        <div> dcpower<span>&#8480;</span>visa&reg;</div>
+        <div></div>
     </div>
 </header>
